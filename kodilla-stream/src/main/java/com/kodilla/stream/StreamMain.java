50af12d6a6105397fcf12f8d95b3e606e23d5ab3
Karol <karol@4-you.pl>
2022-02-21 01:48:48 +0100

    21/02/2022, 01-48, 7.1 Eddited

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,8 +1,25 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+import java.util.Locale;
+
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        //expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+        //expressionExecutor.executeExpression(10, 5, FunctionalCalculator::subBFromA);
+        PoemBeautifier pb = new PoemBeautifier();
+        pb.beautify("Pizza", text -> "Good  " + text + " !!!" );
+        pb.beautify("Burger", text -> "VGood " + text + " ..." );
+        pb.beautify("Kebab", text -> "Not Bad " + text + " ???" );
+        pb.beautify("Sushi", text -> "It's OK " + text + " > <" );
+        pb.beautify("Fries", text -> text.toLowerCase(Locale.ROOT) );
+        pb.beautify("L o L", text -> text.length() + " " + text);
 
     }
 }
