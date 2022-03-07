0b26a46e46bd31d08bd59470c4793570fb80083e
rates336 <37122746+rates336@users.noreply.github.com>
2022-03-07 23:08:30 +0100

    Add files via upload

--- kodilla-stream/src/main/java/com/kodilla/stream/world/Main.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.kodilla.stream.world;
 
 import com.kodilla.stream.world.continents.africa.Africa;
@@ -41,3 +42,48 @@ public class Main {
 
     }
 }
+=======
+package com.kodilla.stream.world;
+
+import com.kodilla.stream.world.continents.africa.Africa;
+import com.kodilla.stream.world.continents.africa.countries.Egypt;
+import com.kodilla.stream.world.continents.africa.countries.RPA;
+import com.kodilla.stream.world.continents.africa.countries.SudanSouth;
+import com.kodilla.stream.world.continents.europe.Europe;
+import com.kodilla.stream.world.continents.europe.countries.Germany;
+import com.kodilla.stream.world.continents.europe.countries.Poland;
+import com.kodilla.stream.world.continents.europe.countries.Ukraine;
+import com.kodilla.stream.world.interfaces.Continent;
+import com.kodilla.stream.world.interfaces.Country;
+
+public class Main {
+
+    /*
+                Description
+     */
+    public void createSelectedCountry(String name) {
+
+    }
+    public void createCountriesSomeoneContinent(Continent continent) {
+
+    }
+
+    public static void creatAllCountries() {
+        Africa africa = new Africa();
+        Europe europe = new Europe();
+    }
+    public void createSelectedContinents(Continent continent) {
+
+    }
+    public static void createAllContinents() {
+        MyWorld world = new MyWorld();
+    }
+    public void createWord() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+>>>>>>> a855bd3207a025fa9c51b0792f51fb44e64e40ed
