55b91b102c80faaad160d6fc3c18f9b8ff347a16
Karol <karol@4-you.pl>
2022-03-21 21:59:21 +0100

    test

--- myPortfolio/src/main/java/com/company/gameHub/computer/Computer.java
+++ myPortfolio/src/main/java/com/company/gameHub/computer/Computer.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.company.gameHub.computer;
 
 import com.company.gameHub.theUserDate.UserModel;
@@ -23,29 +22,3 @@ public class Computer implements UserModel {
         return computerWins + number;
     }
 }
-=======
-package com.company.gameHub.computer;
-
-import com.company.gameHub.theUserDate.UserModel;
-
-public class Computer implements UserModel {
-    private String userName = "Computer";
-    private int age = 2077;
-    int computerWins = 0;
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-    public int userTempWinsUpdate(int number) {
-        return computerWins + number;
-    }
-}
->>>>>>> 77c2a98f1f56e569d4fd4f26e649e8d0068ac57c
