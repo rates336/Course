09259f9b798f8f1d2b30233d9445806b7a95d4eb
Karol <karol@4-you.pl>
2022-03-21 21:43:33 +0100

    test

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.kodilla.stream.array;
 
 public interface ArrayOperations {
@@ -6,22 +5,3 @@ public interface ArrayOperations {
         return 0;
     }
 }
-=======
-<<<<<<< HEAD
-package com.kodilla.stream.array;
-
-public interface ArrayOperations {
-    public static double getAverage(int[] numbers){
-        return 0;
-    }
-}
-=======
-package com.kodilla.stream.array;
-
-public interface ArrayOperations {
-    public static double getAverage(int[] numbers){
-        return 0;
-    }
-}
->>>>>>> 77c2a98f1f56e569d4fd4f26e649e8d0068ac57c
->>>>>>> a855bd3207a025fa9c51b0792f51fb44e64e40ed
--- kodilla-stream/src/main/java/com/kodilla/stream/array/MyArray.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/MyArray.java
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 package com.kodilla.stream.array;
 
 import java.util.stream.IntStream;
-
-
 
 public class MyArray implements ArrayOperations {
     public double getAverage(int[] numbers){
@@ -11,53 +8,10 @@ public class MyArray implements ArrayOperations {
                 .map(e -> numbers[e])
                 .forEach(System.out::println);
         double tabAverage = IntStream.range(0, numbers.length)
-                    .map(e -> numbers[e])
-                    .average()
-                    .getAsDouble();
-        System.out.println(tabAverage);
-        return tabAverage;
-    }
-}
-=======
-<<<<<<< HEAD
-package com.kodilla.stream.array;
-
-import java.util.stream.IntStream;
-
-
-
-public class MyArray implements ArrayOperations {
-    public double getAverage(int[] numbers){
-        IntStream.range(0, numbers.length)
                 .map(e -> numbers[e])
-                .forEach(System.out::println);
-        double tabAverage = IntStream.range(0, numbers.length)
-                    .map(e -> numbers[e])
-                    .average()
-                    .getAsDouble();
+                .average()
+                .getAsDouble();
         System.out.println(tabAverage);
         return tabAverage;
     }
 }
-=======
-package com.kodilla.stream.array;
-
-import java.util.stream.IntStream;
-
-
-
-public class MyArray implements ArrayOperations {
-    public double getAverage(int[] numbers){
-        IntStream.range(0, numbers.length)
-                .map(e -> numbers[e])
-                .forEach(System.out::println);
-        double tabAverage = IntStream.range(0, numbers.length)
-                    .map(e -> numbers[e])
-                    .average()
-                    .getAsDouble();
-        System.out.println(tabAverage);
-        return tabAverage;
-    }
-}
->>>>>>> 77c2a98f1f56e569d4fd4f26e649e8d0068ac57c
->>>>>>> a855bd3207a025fa9c51b0792f51fb44e64e40ed
